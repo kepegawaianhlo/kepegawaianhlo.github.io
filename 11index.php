d5d51d37f032df48862a0a352e5b49a3ff19eb7d
madukubah <muhalfalah1998@gmail.com>
2020-12-25 22:34:53 +0800

    first

---
+++ 11index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>UPBU HALU OLEO KENDARI</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/agency.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark " id="mainNav">
+    <div class="container">
+      <a class="navbar-brand " href="#page-top">UPBU HALU OLEO KENDARI</a>
+      <div class="collapse navbar-collapse" id="xxnavbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+          <li class="nav-item">
+            <a class="nav-link " href="#about_me">Beranda</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section class="bg-light  page-section" id="about_me">
+    <div class="container">
+      <form action="input.php" method="get">
+      <div class="row">
+          <div class="col-lg-12 text-center">
+            <!--  -->
+            <h4>Cancel</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">ARR</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">DEP</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>Extra Flight</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">ARR</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">DEP</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>On Time Performance</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">On Time</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Delayed</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>Penumpang</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>Pesawat</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>Kargo (Kg)</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Tahun</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-4">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <!--  -->
+            <h4>Laporan Kejadian Khusus</h4>
+            <div class="row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Jumlah</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword" >
+                </div>
+            </div>
+            <br><br>
+            <button type="submit" class="btn btn-success" >Kirim</button>
+          </div>
+      </div>
+      </form>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer" id="contact">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-4">
+          <span class="copyright">Get My Contact</span>
+        </div>
+        <div class="col-md-4">
+          <ul class="list-inline social-buttons">
+            <li class="list-inline-item">
+              <a href="https://api.whatsapp.com/send?phone=6281342989185&text=hai+Madukubah" target="_blank" >
+                <i class="fab fa-whatsapp"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/muh-alfalah-198657132/" target="_blank" >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/madukubah" target="_blank" >
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/Alan.theKid" target="_blank" >
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/madukubah/" target="_blank" >
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <span class="copyright">Copyright &copy; Madukubah 2019</span>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Portfolio Modals -->
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Contact form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <!-- <script src="js/agency.min.js"></script> -->
+
+</body>
+
+</html>
